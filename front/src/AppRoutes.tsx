b2d5f93e37e115f0e857e45d9c0ff678b98fef31
Bhanuka Jayaweera <pbhanuka01@gmail.com>
2025-02-23 23:17:30 +0530

    day10

--- front/src/AppRoutes.tsx
+++ front/src/AppRoutes.tsx
@@ -46,7 +46,7 @@ const AppRoutes = () => {
                     element={
                         <Layout>
                             <ManageBookStorePage />
-                        </Layout>
+                        </ Layout>
                     }
                 />
             </Route>
